e7efa3d87469f0c0b69975f86fb5288b8e79b630
PranjayB <130260409+PranjayB@users.noreply.github.com>
2025-02-23 10:31:10 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountID = 98540 ;
+let accountEmail = "pranjay.bhawsar@gmail.com"
+var accountPassword = "PB$2001";
+accountCity = "Mumbai"
+let accountState;
+
+// accountID = 2
+accountEmail= "pranjay.2@gmail.com"
+accountPassword = "PB2001"
+accountCity = "Bangalore"
+
+console.log(accountID);
+console.table([accountEmail,accountID,accountPassword,accountCity,accountState]);
+
+/*
+var should not be used ideally as it does not handle scopes.
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all js code as newer version
+
+// alert('Hello Pranjay'); // cannot be used as we are using nodejs, not browser
+
+// Datatypes:
+
+// number => 2 to power 53
+// bigint
+// null => standalone value
+// undefined => 
+// symbol => to determine uniqueness
+// object
+
+let undefined;
+let no= null;
+
+console.log(typeof("hello"))
+console.log(typeof no);
+console.log(typeof undefined);
+
