ce28889de6b99cc0b74e2f540c5555bb3958ab1b
PranjayB <130260409+PranjayB@users.noreply.github.com>
2025-02-25 16:50:19 +0000

    Conversion Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,8 +1,11 @@
 let score = "33";
-
 console.log(typeof score);
 
 let scoreInNumber = Number(score);
 console.log(scoreInNumber);
 console.log(typeof scoreInNumber);
 
+ let isLoggedIn = 1
+
+ let booleanLoggedIn = Boolean(isLoggedIn);
+ console.log(booleanLoggedIn);
